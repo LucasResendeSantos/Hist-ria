3a5247ff36ca8cc6c947fd5de1a7802415158e81
LucasResendeSantos <lucas.resende.santos15@escola.pr.gov.br>
2026-08-27 09:30:40 -0300

    gf

--- mans.js
+++ mans.js
@@ -89,7 +89,7 @@ function mostraPergunta(){
 }
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.creatElement("button")
+        const botaoAlternativas = document.createElement("button")
         botaoAlternativas.textContent =alternativa.texto
         botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas)
