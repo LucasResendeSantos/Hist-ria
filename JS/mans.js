84e2338f82e22bf5008f951287284868054c38f8
LucasResendeSantos <lucas.resende.santos15@escola.pr.gov.br>
2026-09-22 11:53:37 -0300

    sd

--- JS/mans.js
+++ JS/mans.js
@@ -4,111 +4,7 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
 
-const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola, Hello Kitty encontrou um pequeno gatinho perdido perto do caminho de casa. Ele parecia assustado e não sabia para onde ir. Qual é o primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Vou ajudar o gatinho a encontrar sua família!",
-                afirmacao:["Com muito carinho, decidiu ajudar o gatinho e ficou determinada a encontrar sua família.", 
-                    "No inicio hello kitty ficou muito preocupada, mas se acalmou e tentou manter o gatinho tranquilo"
-                    "Afirmacao 2"
-                ]
-    
 
-            },
-            {
-                texto: "Talvez alguém já esteja procurando por ele.",
-                afirmacao: ["Pensou com cuidado em uma forma segura de ajudar o gatinho sem deixá-lo ainda mais assustado.",
-                    "Ela pensou com cuidado e com carinho para ajudar o gatinho"
-                    "Afirmacao 2"
-                ]
-            }
-        ]
-    },
-    {
-        enunciado: "Depois de encontrar o gatinho, Hello Kitty descobriu que ele adorava brincar, mas estava com saudade de seus amigos. Ela decidiu preparar uma surpresa para animá-lo. O que ela faz?",
-        alternativas: [
-            {
-                texto: "Prepara alguns biscoitinhos em formato de coração.",
-                afirmacao: ["Foi para a cozinha e preparou deliciosos biscoitinhos em formato de coração para deixar o novo amiguinho feliz.",
-                    "Todos os biscoitinhos estavam quentinhos e deliciosos, e fez o caração do gatinho ficar quentinho"
-                    "Afirmacao 2"
-                ]
-            
-            },
-            {
-                texto: "Faz um desenho colorido para ele.",
-                afirmacao: ["Pegou seus lápis de cor e criou um desenho cheio de flores, corações e estrelinhas.",
-                    "Hello Kitty deu um papel e varios lapis de cor para o gatinho de destrair enquanto ela pensava em como ajudar o gatinho"
-                    "Afirmacao 2"
-                ]
-            }
-        ]
-    },
-    {
-        enunciado: "No dia seguinte, Hello Kitty e seus amigos decidiram fazer um piquenique no parque. Cada um deveria levar alguma coisa especial. O que você decide levar?",
-        alternativas: [
-            {
-                texto: "Uma cesta cheia de frutas e docinhos.",
-                afirmacao:[ "Preparou uma linda cesta com frutas, docinhos e vários quitutes para compartilhar com todos.",
-                    "Seus amigos iriam gostar muito das frutas e dos docinhos"
-                    "Afirmacao 2"
-                ]
-            },
-            {
-                texto: "Uma toalha fofinha e jogos para brincar.",
-                afirmacao: ["Levou uma toalha bem confortável e vários jogos divertidos para que todos pudessem brincar juntos.",
-                    "Seria uma boa opção, para todos se divertirem"
-                    "afirmacao 2"
-                ]
-
-
-                
-            }
-        ]
-    },
-    {
-        enunciado: "Durante o piquenique, começou a chover e todos correram para debaixo de uma árvore. Hello Kitty teve uma ideia para deixar aquele momento divertido. E agora?",
-        alternativas: [
-            {
-                texto: "Contar histórias engraçadas para os amigos.",
-                afirmacao: ["Começou a contar histórias engraçadas e logo todos estavam rindo juntos, esquecendo completamente da chuva.",
-                    "Seus amgigos estavam com muitos medo do barulho dos trovões, e como ela é uma ótima amiga decidiu animar os seus amigos"
-                    "afirmacao 2"
-                ]
-            },
-            {
-                texto: "Cantar uma música bem alegre.",
-                afirmacao: ["Começou a cantar uma música alegre e seus amigos entraram na brincadeira, transformando a chuva em uma festa.",
-                    "Ira animar seus amigos e fazer eles esquecerem da chuva"
-                    "afirmacao 2"
-                ]
-            }
-        ]
-    },
-    {
-        enunciado: "Quando a chuva finalmente parou, Hello Kitty percebeu que o gatinho estava triste novamente. Ele queria agradecer por toda a ajuda, mas não sabia como. O que você faz?",
-        alternativas: [
-            {
-                texto: "Diz que não precisa agradecer e dá um abraço nele.",
-                afirmacao: ["Aprendeu que pequenos gestos de carinho podem fazer uma grande diferença e que ajudar um amigo é uma coisa muito especial.",
-                    "Ela tirou um aprendizado com o lindo gesto que ela tomou com o gatinho"
-                    "afirmacao 2"
-                ]
-            },
-            {
-                texto: "Sugere que ele faça uma surpresa para os outros amigos também.",
-                afirmacao: ["Percebeu que a gentileza pode virar uma corrente de carinho quando cada pessoa compartilha um pouquinho de amor com os outros.",
-                    "Ela aprendeu que gentileza pode virar uma corrente de carinho quando cada pessoa compartilha o seu amor"
-                    "afirmacao 2"
-                ]
-
-            
-            }
-        ]
-    },
-];
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = ""
@@ -145,11 +41,7 @@ function mostraResultado(){
     caixaAlternativas.textContent ="";
 }
 
-function aleatorio(lista){
-    cont podicso = Math.floor(Math.random()*lista.length)
-    console.log(posicao)
-    return(lista[posicao])
-}
+
 
 
 mostraPergunta()
