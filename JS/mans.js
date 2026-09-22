f701be094aa6f15da668cdb4359400522af7d33f
LucasResendeSantos <lucas.resende.santos15@escola.pr.gov.br>
2026-09-22 12:17:57 -0300

    L

--- JS/mans.js
+++ JS/mans.js
@@ -1,50 +1,48 @@
 import {aleatorio} from "./aleatorio.js"
 import {perguntas} from "./perguntas.js"
 
-const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaPerguntas = document.querySelector(".caixa-perguntas")
-const caixaAlternativas = document.querySelector(".caixa-alternativas")
-const caixaResultado = document.querySelector(".caixa-resultado")
-const textoResultado = document.querySelector(".texto-resultado")
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 
-let atual = 0;
+let atual = 0; 
 let perguntaAtual;
-let historiaFinal = ""
+let historiaFinal = "";
 
-function mostraPergunta(){
-     if(atual >= perguntas.length){  
-        mostraResultado()
-        return
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
     }
-    perguntaAtual = perguntas[atual]
-    caixaPerguntas.textContent =perguntaAtual.enunciado
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostraAlternativas()
-
-
+    mostraAlternativas();
 }
+
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button")
-        botaoAlternativas.textContent =alternativa.texto
-        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa))
-        caixaAlternativas.appendChild(botaoAlternativas)
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao)
-    historiaFinal += afirmacoes + " "
-    atual++
-    mostraPergunta()
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
 }
+
 function mostraResultado(){
-    caixaPerguntas.textContent ="A partir de sua ajuda ..."
-    textoResultado.textContent = historiaFinal
-    caixaAlternativas.textContent ="";
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = ""; 
 }
 
-
-
-
-mostraPergunta()
+mostraPergunta();
